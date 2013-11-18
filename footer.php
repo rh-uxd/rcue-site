fbeefdf4e12458462a85b61c56834f2bd1022938
Robb Hamilton <rhamilto@redhat.com>
2013-11-18 09:57:57 -0500

    Fixing HTML validation error:   missing closing div

--- footer.php
+++ footer.php
@@ -14,10 +14,11 @@
           </nav>
         </div> <!-- end #inner-footer -->
       </footer> <!-- end footer -->
-      <!--[if lt IE 7 ]>
-        <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-        <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-      <![endif]-->
-      <?php wp_footer(); // js scripts are inserted using this function ?>
+    </div><!--/.container-->
+    <!--[if lt IE 7 ]>
+      <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+      <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+    <![endif]-->
+    <?php wp_footer(); // js scripts are inserted using this function ?>
   </body>
 </html>
